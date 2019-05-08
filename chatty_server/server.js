a0cc143cf28ac877c6b73d1a7ff2a1ade07a9ed4
RachelTWQ <racheltang15@hotmail.com>
2019-05-08 21:20:56 +0000

    add broadcast

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
+const socket = require("ws");
+const uuidv1 = require('uuid/v1');
 
 // Set the port to 3001
 const PORT = 3001;
 
 // Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
@@ -18,13 +20,21 @@ const wss = new SocketServer({ server });
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
-//   ws.on('open', function open() {
-//     ws.send('something');
-//   });
-  
+  //   ws.on('open', function open() {
+  //     ws.send('something');
+  //   });
+  wss.broadcast = function broadcast(data) {
+    wss.clients.forEach((client) => {
+      if (client.readyState === socket.OPEN) {
+        client.send(JSON.stringify(data))
+      }
+    })
+  }
   ws.on('message', function incoming(data) {
     const incomingMsg = JSON.parse(data);
-    console.log(`User ${incomingMsg.username} said ${incomingMsg.content}`);
+    incomingMsg["id"] = uuidv1();
+    // console.log(incomingMsg);
+    wss.broadcast(incomingMsg);
   });
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
